a2da40f0e5cac88d5a0b9f02daa361dafcb7f555
gayathri <gayathriraj.vlr@gmail.com>
2024-07-12 03:04:53 +0530

    interim commit

--- lib/ComponentB.tsx
+++ lib/ComponentB.tsx
@@ -1,15 +1,26 @@
-import { Component1, UserContext } from "z-library-a"
-import ComponentC from "./ComponentC"
+import { Component1, UserContext, accessTokenVar } from "z-library-a"
 import { useContext } from "react"
+import { useReactiveVar } from "@apollo/client";
 
 
 const ComponentB = () => {
-  const user = useContext(UserContext)
+  const user = useContext(UserContext);
+  const accessTokenData = useReactiveVar(accessTokenVar);
+
   return (
     <Component1>
         <div>Component B - Library B</div>
         <div>ComponentB - Lib B {user}</div>
-        <ComponentC />
+        {/* <ComponentC /> */}
+
+        <div>
+         {/* <UpdateButton /> */}
+
+          <p>Lib b:: {JSON.stringify(accessTokenVar())}</p>
+          <p>Access Token: {accessTokenData.accessToken}</p>
+          <p>Role: {accessTokenData.claims.role}</p>
+          <p>Sub: {accessTokenData.claims.sub}</p>
+        </div>
     </Component1>
   )
 }
